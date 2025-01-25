9f80ba07fc4d5f6b5753ba90f1c96fc20d653deb
tugudush <jerome2kph@gmail.com>
2025-01-25 10:31:53 +0800

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,8 @@ function App() {
   return (
     <main className="container mx-auto p-4 max-w-6xl">
       <h1 className="text-3xl font-bold mb-8">Focus Flow</h1>
-      <Form onSubmit={handleAddItem}>
-        <List items={items} onDelete={handleDeleteItem} />
-      </Form>
+      <Form onSubmit={handleAddItem} />
+      <List items={items} onDelete={handleDeleteItem} />
     </main>
   );
 }
